5556006bb26f7cdb43c7c6fc5ad5756fa7f01413
lmsz09 <leamsi.lopes@escola.pr.gov.br>
2026-05-01 01:49:35 +0000

    atividade 2 finalizada

---
+++ aula-2/array.js
@@ -0,0 +1,48 @@
+const listaLivros = [
+  {
+    titulo: "Sombras do Amanhã",
+    preco: 15
+  },
+  {
+    titulo: "O Último Horizonte",
+    preco: 22
+  },
+  {
+    titulo: "Entre Cinzas e Estrelas",
+    preco: 30
+  },
+  {
+  titulo: "Vozes do Silêncio",
+  preco: 18
+  },
+  {
+    titulo: "A Chave do Destino",
+    preco: 27
+  },
+  {
+    titulo: "Fragmentos de Eternidade",
+    preco: 35
+  },
+  {
+    titulo: "O Labirinto de Vidro",
+    preco: 40
+  },
+  {
+    titulo: "Coração de Ferro",
+    preco: 12
+  },
+  {
+    titulo: "Além do Véu",
+    preco: 25
+  },
+  {
+    titulo: "Ecos Perdidos",
+    preco: 33
+  },
+  {
+    titulo: "O Guardião das Horas",
+    preco: 45
+  },
+]
+
+module.exports = listaLivros
